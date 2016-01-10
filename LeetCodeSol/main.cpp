cd2a5f7422e47a8deb0b7f6f31ae1aea7c0ab319
lei xinye <xinyelei@xinyedemacbook-pro.local>
2016-01-09 20:25:41 -0500

    Initial Commit

---
+++ LeetCodeSol/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeetCodeSol
+//
+//  Created by xinye lei on 16/1/9.
+//  Copyright © 2016年 xinye lei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
